b6a6f93bd890653bf910227a2b72d221f4269ade
Dyious <43286178+Dyious@users.noreply.github.com>
2020-08-18 15:14:05 +0800

    Add files via upload

---
+++ src/components/Paginate.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-flex align-item-center justify-content-start my-3">
+    <div class="">
+      <button
+        type="button"
+        class="btn btn-light"
+        @click="changePage(pageData.current_page - 1)"
+      >&lt;
+      </button>
+    </div>
+    <div
+      class=""
+      v-for="(item,index) in pageData.total_pages"
+      :key="index"
+    >
+      <button
+        type="button"
+        class="btn btn-light"
+        :class="{'active': item === pageData.current_page}"
+        @click="changePage(item )"
+      >{{item }}
+      </button>
+    </div>
+    <div class="">
+      <button
+        type="button"
+        class="btn btn-light"
+        @click="changePage(pageData.current_page + 1)"
+      >&gt;
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Paginate',
+  props: {
+    pageData: {
+      type: Object,
+      default () {
+        return {
+        }
+      }
+    }
+  },
+  methods: {
+    changePage (item) {
+      this.$emit('changePage', item)
+    }
+  }
+}
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbor-height bg-place">
+    <ul class="nav justify-content-around">
+      <li>
+        <router-link to="/dashboard/products">產品列表</router-link>
+      </li>
+      <li>
+        <router-link to="/dashboard/coupons">優惠券列表</router-link>
+      </li>
+      <li>
+        <router-link to="/dashboard/orders">訂單列表</router-link>
+      </li>
+      <li>
+        <router-link to="/dashboard/imageCatch">圖片儲存列表</router-link>
+      </li>
+      <li>
+        <a
+          href="#"
+          @click.prevent='signOut()'
+        >登出</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+
+export default {
+  name: 'Sidebar',
+  methods: {
+    signOut () {
+      if (this.$cookie.get('token') !== undefined) {
+        this.$cookie.delete('token')
+        this.$router.push('/login')
+      }
+    }
+  }
+}
+</script>
