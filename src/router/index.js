2c94a451fc12b82360726e89a15addb83475b577
Dyious <43286178+Dyious@users.noreply.github.com>
2020-08-18 15:16:17 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,40 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/dashboard/Dashboard.vue'),
+    children: [
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('../views/dashboard/Products.vue')
+      },
+      {
+        path: 'coupons',
+        name: 'Coupons',
+        component: () => import('../views/dashboard/Coupons.vue')
+      },
+      {
+        path: 'orders',
+        name: 'Orders',
+        component: () => import('../views/dashboard/Orders.vue')
+      },
+      {
+        path: 'imageCatch',
+        name: 'ImageCatch',
+        component: () => import('../views/dashboard/ImageCatch.vue')
+      }
+    ]
+  },
+  {
+    path: '*',
+    redicect: '/'
   }
 ]
 
